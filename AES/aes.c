777692856e459fa514313cc952a058a5b76bc355
James Gross <fgjames9@gmail.com>
2013-11-23 13:31:43 -0500

    Forgot to add files in last commit

--- AES/aes.c
+++ AES/aes.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "aes.h"
 
 bool 
@@ -38,15 +40,29 @@ AES_KeySchedule
 
 }
 
-uint8_t* 
+bool 
 AES_Encrypt
 ( uint8_t* plaintext, uint8_t* key )
 {
+    AES_AddRoundKey(plaintext, key, 0);
 
+    int i;
+    for(i = 1; i < NUM_ROUNDS - 1; i++)
+    {
+        AES_Subbytes(plaintext);
+        AES_ShiftRows(plaintext);
+        AES_MixColumns(plaintext);
+        AES_AddRoundKey(plaintext, key, i);
+    }
+    
+    /* Last Round */
+    AES_Subbytes(plaintext);
+    AES_ShiftRows(plaintext);
+    AES_AddRoundKey(plaintext, key, NUM_ROUNDS - 1);
 }
 
 
-uint8_t* 
+bool
 AES_Decrypt
 ( uint8_t* ciphertext, uint8_t* key )
 {
--- AES/aes.h
+++ AES/aes.h
@@ -4,17 +4,29 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define NUM_ROUNDS 10
+
 typedef uint32_t polynomial;
 
-bool extended_gcd(polynomail a, polynomial b, uint32_t modulus);
+bool gcd_extended_polynomial(polynomail a, polynomial b, uint32_t modulus);
 
 bool AES_Subbytes(uint8_t* input);
 bool AES_ShiftRows(uint8_t* input);
 bool AES_MixColumns(uint8_t* input);
-bool AES_AddRoundKey(uint8_t* input);
-bool AES_KeySchedule(uint8_t* key, uint8_t* round_key);
+bool AES_AddRoundKey(uint8_t* input, uint8_t* key, uint32_t round);
 
-uint8_t* AES_Encrypt(uint8_t* plaintext, uint8_t* key);
-uint8_t* AES_Decrypt(uint8_t* ciphertext, uint8_t* key); 
+/*
+ * input array structure by column:
+ * input[0]     input[1]    input[2]    input[3]
+ * |            |           |           |
+ * V            V           V           V
+ * b0           b4          b8          b12
+ * b1           b5          b9          b13
+ * b2           b6          b10         b14
+ * b3           b7          b11         b15
+ */
+
+bool AES_Encrypt(uint8_t* plaintext, uint8_t* key);
+bool AES_Decrypt(uint8_t* ciphertext, uint8_t* key); 
 
 #endif /* AES_H */
